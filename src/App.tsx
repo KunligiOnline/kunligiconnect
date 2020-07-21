3dd30089e97e3ca64e60e37577ccdcc1f485383c
crperezt <crperez@gmail.com>
2020-07-20 20:04:58 -0700

    changing package.json and imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/login/Login';
 import Chat from './components/chat/Chat';
