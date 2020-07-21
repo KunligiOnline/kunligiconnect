a01dd3d1680b31e5f234f7c34d9bab6b06053c96
Tyler Sullberg <tysullberg@gmail.com>
2020-07-21 10:04:41 -0700

    Implemented proof of concept for websockets

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 4000;
+// server.listen(3000, '127.0.0.1');
 
 app.use(express.json());
 
@@ -15,15 +16,16 @@ const cookieController = require('./controllers/cookieController');
 const userController = require('./controllers/userController');
 const chatRouter = require('./routes/chats');
 const userRouter = require('./routes/users');
+const cors = require('cors');
 
 // catch-all route handler for any requests to an unknown route
+app.use(cors());
+io.set('origins', '*:*');
 
 app.use('/chat', chatRouter);
 app.use('/user', userRouter);
 
 app.get('/test', (req, res) => res.status(200).send('hello'));
-
-app.use((req, res) => res.sendStatus(404));
 
 /**
  * express error handler
@@ -40,13 +42,39 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
+
+let interval;
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('New client connected');
+  // if there is an interval already, clear it so you don't flood the server
+  if (interval) {
+    clearInterval(interval);
+  }
+  // Set an interval that will send the date every second
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  // clear interval on disconnect to avoid flooding the server
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    clearInterval(interval);
+  });
 });
 
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit('FromAPI', response);
+};
+
+app.use((req, res) => res.sendStatus(404));
 // start server
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port: ${PORT}`);
+// });
+
+server.listen(4000, '127.0.0.1');
 
 module.exports = app;
--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../navigation/Navbar';
 import Messages from '../chat/Messages';
-import MessageInput from '../chat/MessageInput'
+import MessageInput from '../chat/MessageInput';
+import socketIOClient from 'socket.io-client';
+// const ENDPOINT = 'http://127.0.0.1:4001';
+// const ENDPOINT = 'http://localhost:4000/';
 
 // will inlcude outes to home page and next question and next user
 const Chat: React.FC = () => {
-    return(
-        <div>
-            <Navbar/>
-            <div>
-                <div>
-                    <p>Question here</p>
-                </div>
-                <Messages/>
-                <MessageInput/>
-            </div>
+  //   const [loadClient, setLoadClient] = useState(true);
+  const [response, setResponse] = useState('');
 
+  useEffect(() => {
+    // const socket = socketIOClient(ENDPOINT);
+    const socket = socketIOClient('http://localhost:4000', {
+      transports: ['websocket'],
+    });
+    console.log('socket done');
+    socket.on('FromAPI', (data: any) => {
+      setResponse(data);
+      console.log(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <div>
+          <p>Question here</p>
+          {/* <button onClick={() => setLoadClient((prevState) => !prevState)}>
+            TEST
+          </button> */}
+          <p>
+            It's <time dateTime={response}>{response}</time>
+          </p>
         </div>
-    )
-}
+        <Messages />
+        <MessageInput />
+      </div>
+    </div>
+  );
+};
 
 export default Chat;
