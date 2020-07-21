e7b4eddae176e2351c2495a50c5d2cca09f96778
Tyler Sullberg <tysullberg@gmail.com>
2020-07-20 20:27:41 -0700

    Set up routes for user and chat endpoints

--- server/server.js
+++ server/server.js
@@ -2,16 +2,27 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
 const PORT = process.env.PORT || 4000;
 
+app.use(express.json());
+
 // import controllers
-const chatController = require('./controllers/starWarsController');
+const chatController = require('./controllers/chatController');
 const cookieController = require('./controllers/cookieController');
 const userController = require('./controllers/userController');
+const chatRouter = require('./routes/chats');
+const userRouter = require('./routes/users');
+
+// catch-all route handler for any requests to an unknown route
+
+app.use('/chat', chatRouter);
+app.use('/user', userRouter);
 
 app.get('/test', (req, res) => res.status(200).send('hello'));
 
-// catch-all route handler for any requests to an unknown route
 app.use((req, res) => res.sendStatus(404));
 
 /**
@@ -27,6 +38,10 @@ app.use((err, req, res, next) => {
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 // start server
