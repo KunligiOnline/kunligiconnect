26ed47e6ab9e5d9e290df94dca5e602577f7e8ce
crperezt <crperez@gmail.com>
2020-07-20 18:37:32 -0700

    setting up redux state management

---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+function Login() {
+  return (
+    <div className="Login">
+      <header className="App-header">
+        <h1> Kunligi </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Login;
